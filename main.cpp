d85ce9f9789c6dd4d399d4599c3559704854f3bb
tomstao <96sutao@gmail.com>
2024-11-06 07:42:46 -0500

    inorder and postorder algorithms added

--- main.cpp
+++ main.cpp
@@ -37,6 +37,25 @@ void preorder(Node* node)
     preorder(node->left);
     preorder(node->right);
 }
+
+void inorder(Node* node)
+{
+    if (node == nullptr)
+        return;
+    inorder(node->left);
+    std::cout << node->data << " ";
+    inorder(node->right);
+}
+
+void postorder(Node* node)
+{
+    if (node == nullptr)
+        return;
+    postorder(node->left);
+    postorder(node->right);
+    std::cout << node->data << " ";
+}
+
 int main()
 {
     Node* root = newNode(1);
@@ -54,6 +73,13 @@ int main()
 
     std::cout << "Preorder:" <<std::endl;
     preorder(root);
+    std::cout << std::endl;
+    std::cout << "Inorder:" <<std::endl;
+    inorder(root);
+    std::cout << std::endl;
+    std::cout << "Postorder:" <<std::endl;
+    postorder(root);
+    std::cout << std::endl;
 
     deleteTree(root);
     return 0;
