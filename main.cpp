374d2d9cc5d0d56a2010847de166be7b60b44dcc
tomstao <96sutao@gmail.com>
2024-11-06 07:29:28 -0500

    added more nodes

--- main.cpp
+++ main.cpp
@@ -32,12 +32,19 @@ void deleteTree(Node* node)
 int main()
 {
     Node* root = newNode(1);
+    // level 1
     root -> left = newNode(2);
     root -> right = newNode(3);
+    // level 2
     root -> left -> left = newNode(4);
-    root -> right -> right = newNode(5);
-    root -> left -> left -> left = newNode(6);
-    root -> right -> right -> right = newNode(7);
+    root -> left -> right = newNode(5);
+    root -> right -> left = newNode(6);
+    root -> right -> right = newNode(7);
+    // level 3
+    root -> left -> right -> left = newNode(9);
+    root -> right -> right -> left = newNode(15);
+
+
     deleteTree(root);
     std::cout << "Hello, World!" << std::endl;
     return 0;
